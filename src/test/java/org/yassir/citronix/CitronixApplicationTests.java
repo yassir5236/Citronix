e42b3a3d64eba0f4bd907effd1fc5cca2dedf0ce
yassir5236 <yassiraitlahmadi@gmail.com>
2024-11-16 14:58:15 +0100

    first commit

---
+++ src/test/java/org/yassir/citronix/CitronixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yassir.citronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitronixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
