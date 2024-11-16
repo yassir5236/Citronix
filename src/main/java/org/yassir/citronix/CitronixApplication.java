45386249d03616dd091b3d21987413d4b52a0049
yassir5236 <yassiraitlahmadi@gmail.com>
2024-11-16 15:46:55 +0100

    adding verification CIT-36

--- src/main/java/org/yassir/citronix/CitronixApplication.java
+++ src/main/java/org/yassir/citronix/CitronixApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CitronixApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello word");
         SpringApplication.run(CitronixApplication.class, args);
     }
 
